a76af476775a712e54aa0f886fb8da68764f7aed
Chris Tam <ctam86@uwo.ca>
2019-04-04 22:57:43 -0400

    first commit

---
+++ heap.java
@@ -0,0 +1,48 @@
+/**
+* Student Name: Christopher Tam
+* Student ID: 250803892
+**/
+public class heap<T> {
+  private int [] initial_array;
+  private int [] heap_array;
+  private int max_size;
+  private int curr_size;
+
+  // Constructure for heap creation
+  public heap(int [] keys, int n) {
+
+
+  }
+
+  // Return true if an id is in the heap
+  public boolean inHeap(int id) {
+
+
+  }
+
+  // Return the minimum key in the heap
+  public int getMinKey() {
+
+  }
+
+  // Return the minimum id in the heap
+  public int getMinId() {
+
+
+  }
+
+  // Return the key of the requested id
+  public int getKey(int id) {
+
+  }
+
+  // Delete the minimum key element in the heap
+  public int deleteMin() {
+
+  }
+
+  // Search heap for an element with the requested id and set the element to new key
+  public void decreaseKey(int id, int newKey) {
+    
+  }
+}
---
+++ node.java
@@ -0,0 +1,10 @@
+public class node<T> {
+  int weight;
+  int vertex;
+
+  // Nodes to be used in heap class
+  public node(int weight, int vertex) {
+    this.weight = weight;
+    this.vertex = vertex;
+  }
+}
